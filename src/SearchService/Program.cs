114527bb5f45d1a42621b71d2dcf635a1cea6085
Yerzhan Kalzhani <yerzhan.kalzhani@clevemed.com>
2026-06-22 19:48:25 +0500

    End of Appendix B

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using MassTransit;
+using MongoDB.Entities;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService.Consumers;
@@ -64,14 +65,9 @@ app.MapControllers();
 
 app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    try
-    {
-        await DbInitializer.InitDb(app);
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e);
-    }
+    await Policy.Handle<TimeoutException>()
+        .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(10))
+        .ExecuteAndCaptureAsync(async () => await DbInitializer.InitDb(app));
 });
 
 
